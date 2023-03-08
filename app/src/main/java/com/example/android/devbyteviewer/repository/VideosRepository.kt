f77fd76654b621753e3d04dc412dea2c99761b01
Samuel Bourassa <district67@gmail.com>
2023-03-07 19:05:44 -0500

    First commit. Start to implement VideosRepository class.

---
+++ app/src/main/java/com/example/android/devbyteviewer/repository/VideosRepository.kt
@@ -0,0 +1,34 @@
+/*
+ * Copyright (C) 2019 Google Inc.
+ *gi
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.android.devbyteviewer.repository
+
+import com.example.android.devbyteviewer.database.VideosDatabase
+import com.example.android.devbyteviewer.network.DevByteNetwork
+import com.example.android.devbyteviewer.network.asDatabaseModel
+
+/**
+ * Repository for fetching devbyte videos from the network and storing them on disk
+ */
+// TODO: Implement the VideosRepository class
+class VideosRepository(private val database: VideosDatabase) {
+    suspend fun refreshVideos() {
+        val playlist = DevByteNetwork.devbytes.getPlaylist()
+        database.videoDao.insertAll(playlist.asDatabaseModel())
+    }
+
+}
+
